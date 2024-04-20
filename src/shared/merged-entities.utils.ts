aadc23bf5c5a743e14af3106cd261671b26d30e4
mohaimenmahi <mmc.dp11@gmail.com>
2024-04-20 23:14:35 +0600

    fix: unused file deleted

--- src/shared/merged-entities.utils.ts
+++
@@ -1,6 +0,0 @@
-import { Item } from "src/items/entities/item.entity";
-import { Comment } from "src/items/entities/comment.entity";
-import { Listing } from "src/items/entities/listing.entity";
-import { Tag } from "src/items/entities/tag.entity";
-
-export const entities = [Item, Comment, Listing, Tag]
