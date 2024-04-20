063ce6bd40bf7d83b9208505f64d2345badf65cf
mohaimenmahi <mmc.dp11@gmail.com>
2024-04-21 00:12:53 +0600

    feat: unit testing added

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsService } from './items.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
+import { EntityManager, Repository } from 'typeorm';
 
 describe('ItemsService', () => {
   let service: ItemsService;
+  let itemRepository: Repository<Item>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ItemsService],
+      providers: [
+        ItemsService,
+        {
+          provide: getRepositoryToken(Item),
+          useValue: {
+            find: jest.fn()
+          },
+        },
+        {
+          provide: EntityManager,
+          useValue: {},
+        }
+      ],
     }).compile();
 
     service = module.get<ItemsService>(ItemsService);
+    itemRepository = module.get<Repository<Item>>(getRepositoryToken(Item));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('findAll', async () => {
+    await service.findAll();
+    expect(itemRepository.find).toHaveBeenCalled();
+  }) 
 });
